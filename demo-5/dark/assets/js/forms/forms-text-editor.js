4e04bf8f2236b146a20e8810ef04ffeb406baa8c
mohanshetty <mohanshetty2019@gmail.com>
2022-05-30 17:47:12 +0530

    new files

---
+++ demo-5/dark/assets/js/forms/forms-text-editor.js
@@ -0,0 +1,10 @@
+// Markdown Editor
+new SimpleMDE({
+	element: document.getElementById("demo1"),
+	spellChecker: false,
+});
+
+// Quill Editor
+var quill = new Quill('#editor', {
+  theme: 'snow'
+});
